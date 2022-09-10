1427d1b5e125e26c77a6fc52bd85258af60cb746
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-09-11 02:52:01 +1000

    Fun experiment

--- cipher999/src/main.rs
+++ cipher999/src/main.rs
@@ -1,16 +1,37 @@
+use rand::Rng;
 use std::io;
 
-fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
+fn user_argument(message: &str) -> String {
+    println!("{message}");
+    let mut input = String::new();
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut input)
         .expect("Failed to read line");
+    return input;
+}
 
-    guess = "7".to_string();
+fn get_number() -> u32 {
+    let message: &str = "Enter integer:";
+    let input = loop {
+        match user_argument(message).trim().parse::<u32>() {
+            Ok(input) => break input,
+            Err(e) => println!("Enter valid number!\n{e}\n"),
+        }
+    };
+    return input;
+}
 
-    println!("You guessed: {guess}");
+fn print_random_number() {
+    let number: u32 = rand::thread_rng().gen_range(u32::MIN..u32::MAX);
+    println!("{number}")
+}
+
+fn main() {
+    loop {
+        let n: u32 = get_number();
+        for _ in 0..n {
+            print_random_number();
+        }
+        println!("\n")
+    }
 }
