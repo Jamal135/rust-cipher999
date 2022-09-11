c5a1822c9378289314a25c6df01caf31d4cbfee7
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-09-12 00:36:31 +1000

    Progress developing fun functions

--- cipher999/src/main.rs
+++ cipher999/src/main.rs
@@ -1,6 +1,22 @@
+#![allow(dead_code)] // Temporary
+use rand::distributions::uniform::SampleUniform;
+use rand::distributions::Uniform;
 use rand::Rng;
+use std::cmp::PartialOrd;
+use std::fmt::Display;
+use std::fs;
 use std::io;
+use std::str::FromStr;
 
+// Creation Date: 10/09/2022
+
+// Load file at file path into u8 vector of bytes
+fn load_file_bytes(file_path: &str) -> Vec<u8> {
+    let contents = fs::read_to_string(file_path).expect("Unable to read file");
+    return contents.into_bytes();
+}
+
+// Prints message argument and returns user input
 fn user_argument(message: &str) -> String {
     println!("{message}");
     let mut input = String::new();
@@ -10,10 +26,14 @@ fn user_argument(message: &str) -> String {
     return input;
 }
 
-fn get_number() -> u32 {
+// Get and verify user input as specified integer type
+fn get_number<T: Display + PartialOrd + SampleUniform + FromStr>() -> T
+where
+    <T as FromStr>::Err: Display,
+{
     let message: &str = "Enter integer:";
     let input = loop {
-        match user_argument(message).trim().parse::<u32>() {
+        match user_argument(message).trim().parse::<T>() {
             Ok(input) => break input,
             Err(e) => println!("Enter valid number!\n{e}\n"),
         }
@@ -21,17 +41,22 @@ fn get_number() -> u32 {
     return input;
 }
 
-fn print_random_number() {
-    let number: u32 = rand::thread_rng().gen_range(u32::MIN..u32::MAX);
-    println!("{number}")
+// Get random number between min and max as specified integer type
+fn random_number<T: Display + PartialOrd + SampleUniform>(min: T, max: T) -> T {
+    let random_number: T = rand::thread_rng().gen_range(min..max);
+    return random_number;
+}
+
+// Get random numbers vector between min and max of length as specified integer
+fn random_vector<T: Display + PartialOrd + SampleUniform>(min: T, max: T, length: u32) -> Vec<T> {
+    let range = Uniform::from(min..max);
+    let mut random = rand::thread_rng();
+    return (0..length).map(|_| random.sample(&range)).collect();
 }
 
 fn main() {
-    loop {
-        let n: u32 = get_number();
-        for _ in 0..n {
-            print_random_number();
-        }
-        println!("\n")
-    }
+    let bytes = load_file_bytes("test.txt");
+    println!("{:?}", bytes);
+    let number = get_number::<u32>();
+    println!("{number}");
 }
